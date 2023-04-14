822598c3cb5047d87b426a1c678f985a5092ab14
dihoon <dihoon955@gmail.com>
2023-04-15 04:30:06 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ import { router } from "./router";
 import "the-new-css-reset";
 import { worker } from "./mocks/worker";
 
-worker.start();
+if (process.env.NODE_ENV === "production") {
+  worker.start({
+    serviceWorker: {
+      url: './mockServiceWorker.js'
+    }
+  });
+} else {
+  worker.start();
+}
 
 const queryClient = new QueryClient();
 
